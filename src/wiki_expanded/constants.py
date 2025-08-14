4fd1cf4c3b6edf8b0caec8cf47d7ec161fc2ea83
Oliver Kinch <oliver.kinch@alexandra.dk>
2025-08-14 14:51:30 +0200

    Store original title

--- src/wiki_expanded/constants.py
+++ src/wiki_expanded/constants.py
@@ -5,7 +5,7 @@ FILE_NAMES = {
     "title_to_text": "title_to_text.json",
     "link_to_freq": "link_to_freq.json",
     "title_to_tokens": "title_to_tokens.json",
-    "title_lower_to_original": "title_lower_to_original.json",
+    "original_title": "original_title.json",
     "dataset": "dataset.jsonl",
     "link_expansion_count": "link_expansion_count.json",
 }
